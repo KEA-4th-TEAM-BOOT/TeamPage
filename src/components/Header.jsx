0e69bb767afdbe6b5b769f058488e663ea8d917f
Gyuwon-Jung <licmm0520@gmail.com>
2024-03-18 14:33:42 +0900

    Back Button Complete

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,18 @@
 import * as React from "react";
-
+import { useNavigate } from "react-router-dom";
 const Header = () => {
+  const navigate = useNavigate();
+  const onClickBtn = () => {
+    navigate(-1); // 바로 이전 페이지로 이동, '/main' 등 직접 지정도 당연히 가능
+  };
   return (
-    <header>
-    <img
-      loading="lazy"
-      src="https://cdn.builder.io/api/v1/image/assets/TEMP/b67eb3708e53c209b4b8a64a293fa4b27084bb419dad02ec5f6f01bce1b3e067?apiKey=a9a9d68966df47cab33790d709ea20f1&"
-      className="shrink-0 self-start border-solid aspect-[1.23] border-[9px] border-black border-opacity-40 stroke-[8.66px] stroke-black stroke-opacity-40 w-[79px]"
-    />
-    </header>
+    <button type="button" class="w-full flex items-center justify-center w-1/2 px-5 py-2 text-sm text-gray-700 transition-colors duration-200 bg-white rounded-lg sm:w-auto dark:hover:bg-gray-800 dark:bg-gray-900 hover:bg-gray-100 "
+    onClick={onClickBtn}>
+    <svg class="w-5 h-5 rtl:rotate-180" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18" />
+    </svg>
+    <span></span>
+  </button>
   );
 }
 
